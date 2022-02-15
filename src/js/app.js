9e95cc18ed518d3d18991f96ce22c98fc08e5c8e
Vasilena <vt.merica@gmail.com>
2022-02-15 14:14:11 +0200

    add js in new file

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
- 
+ var product = document.querySelectorAll('.product');
+ var price = document.querySelectorAll('.price')
+ product.setAttribute('data-price', price); 
 
 });
