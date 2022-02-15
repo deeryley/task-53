69e707ad3e30891e6698dbb91d5127ad5e4399ab
Vasilena <vt.merica@gmail.com>
2022-02-15 14:04:55 +0200

    new

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,6 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
-  const product1 = document.querySelectorAll('.product');
-  const price1 = document.querySelectorAll('.price');
-  product1.setAttribute('data-price', price1);
+ 
 
 });
