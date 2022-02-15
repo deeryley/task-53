a1a7a525fef5be5fddeffe182811c8a1110a3895
Vasilena <vt.merica@gmail.com>
2022-02-15 13:48:41 +0200

    add js

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,8 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
+  const product1 = document.querySelector('.product');
+  const price1 = document.querySelector('.price');
+  product1.setAttribute('data-price', price1);
 
 });
